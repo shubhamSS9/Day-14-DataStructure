93530256f37dea5ca1c0e07d3f06f9a48a24cd89
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-24 21:21:45 +0530

    added-UC4_LinkedList_Insert_Method

--- src/com/Bridglabz/LinkedListMain.java
+++ src/com/Bridglabz/LinkedListMain.java
@@ -4,7 +4,6 @@ public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>();
         linkedList.add(56);
-        linkedList.add(30);
         linkedList.add(70);
         linkedList.display();
 //        LinkedList<Integer> linkedList1 = new LinkedList<>();
@@ -13,6 +12,15 @@ public class LinkedListMain {
 //        linkedList1.push(56);
 //        linkedList1.display();
 
-
+        		boolean isInserted = linkedList.insertAfter(56, 30);
+		if (isInserted)
+			System.out.println("Element inserted successfully!");
+		else
+			System.out.println("Element not inserted!");
+        linkedList.display();
     }
 }
+
+
+
+
