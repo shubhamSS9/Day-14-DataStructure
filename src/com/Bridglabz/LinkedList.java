6cb4211fd987f33ca42c91f232b38d93108cd9a8
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-25 19:58:31 +0530

    added-UC9_Delete_40_From_List

--- src/com/Bridglabz/LinkedList.java
+++ src/com/Bridglabz/LinkedList.java
@@ -2,6 +2,7 @@ package com.Bridglabz;
 
 public class LinkedList<T> {
 
+
     Node<T> head;
     Node<T> tail;
 
@@ -14,6 +15,14 @@ public class LinkedList<T> {
             tail.next = newNode;
             tail = newNode;
         }
+
+        Node currentNode = this.head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = newNode;
+
     }
 
 //	public T pop() {
@@ -24,22 +33,22 @@ public class LinkedList<T> {
 //		return popData;
 //	}
 
-//	public T popLast() {
-//		if (head == null)
-//			return null;
-//		T popData = tail.data;
-//		if (head == tail) {
-//			head = null;
-//			return popData;
-//		}
-//		Node<T> temp = head;
-//		while (temp.next != tail) {
-//			temp = temp.next;
-//		}
-//		temp.next = null;
-//		tail = temp;
-//		return popData;
-//	}
+    public T popLast() {
+        if (head == null)
+            return null;
+        T popData = tail.data;
+        if (head == tail) {
+            head = null;
+            return popData;
+        }
+        Node<T> temp = head;
+        while (temp.next != tail) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        tail = temp;
+        return popData;
+    }
 
     public Node<T> search(T searchData) {
         Node<T> temp = head;
@@ -51,21 +60,22 @@ public class LinkedList<T> {
         return null;
     }
 
-    public boolean insertAfter(T searchData, T insertData) {
-        Node<T> newNode = new Node<>(insertData);
-        Node<T> searchedNode = search(searchData);
-        if (searchedNode != null) {
-            newNode.next = searchedNode.next;
-            searchedNode.next = newNode;
-            return true;
-        }
-        return false;
-    }
+
+//	public boolean insertAfter(T searchData, T insertData) {
+//		Node<T> newNode = new Node<>(insertData);
+//		Node<T> searchedNode = search(searchData);
+//		if (searchedNode != null) {
+//			newNode.next = searchedNode.next;
+//			searchedNode.next = newNode;
+//			return true;
+//		}
+//		return false;
+//	}
 
     public void show() {
         Node<T> temp = head;
         while (temp != null) {
-            System.out.print(temp.data + " -> ");
+            System.out.print(temp.getData() + " -> ");
             temp = temp.next;
         }
         System.out.println();
--- src/com/Bridglabz/LinkedListMain.java
+++ src/com/Bridglabz/LinkedListMain.java
@@ -6,25 +6,27 @@ public class LinkedListMain {
         linkedList1.append(56);
         linkedList1.append(30);
         linkedList1.append(70);
+        linkedList1.append(40);
 
-//		Integer poppedData1 = linkedList1.popLast();
-//		if (poppedData1 == null)
-//			System.out.println("Linked List is Empty");
-//		else
-//			System.out.println("The element popped is => " + poppedData1);
-//
-//		Node<Integer> searchedData = linkedList1.search(30);
-//        if (searchedData == null) {
-//            System.out.println("Element not found!");
-//        } else {
-//            System.out.println("Element Found");
-//        }
 
-        boolean isInserted = linkedList1.insertAfter(30, 40);
-        if (isInserted)
-            System.out.println("Element inserted successfully!");
+        Node<Integer> searchedData = linkedList1.search(30);
+        if (searchedData == null) {
+            System.out.println("Element not found!");
+        } else {
+            System.out.println("Element Found");
+        }
+
+        Integer poppedData1 = linkedList1.popLast();
+        if (poppedData1 == null)
+            System.out.println("Linked List is Empty");
         else
-            System.out.println("Element not inserted!");
+            System.out.println("The element popped is => " + poppedData1);
+
+//		boolean isInserted = linkedList1.insertAfter(30, 40);
+//		if (isInserted)
+//			System.out.println("Element inserted successfully!");
+//		else
+//			System.out.println("Element not inserted!");
         linkedList1.show();
     }
 }
